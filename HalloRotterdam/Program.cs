379564cb4fec0b7ef444241256b4307d34be44d9
deadwalker010 <75258515+deadwalker010@users.noreply.github.com>
2021-03-22 14:15:16 +0100

    Update Program.cs

--- HalloRotterdam/Program.cs
+++ HalloRotterdam/Program.cs
@@ -7,7 +7,7 @@ namespace HalloRotterdam
         static void Main(string[] args)
         {
             Console.WriteLine("Hallo Rotterdam!");
-            Console.WriteLine("Groeten van de pannenkoekenboot");
+            Console.WriteLine("Groet van de pannenkoekenboot");
         }
     }
 }
